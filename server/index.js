f47705f3df6cf5a3e3c741d2936e0a46cf0065c9
HunterX18 <srsahu13@gmail.com>
2024-01-19 22:14:31 +0530

    testing vercel

--- server/index.js
+++ server/index.js
@@ -10,6 +10,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get("/", (req, res) => {
+	res.json("Server is running");
+});
+
 app.use("/api/support-agents", SupportAgentRoutes);
 app.use("/api/support-tickets", SupportTicketRoutes);
 
